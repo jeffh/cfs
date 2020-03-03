a554d33988ca6eea40131c3af7b0d024862b4b92
Jeff Hui <jeff@jeffhui.net>
2020-03-03 09:23:07 -0800

    Add tmpfs, a derrivative of dirfs

---
+++ cmd/tmpfs/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/jeffh/cfs/cli"
+	"github.com/jeffh/cfs/fs"
+	"github.com/jeffh/cfs/ninep"
+	"github.com/kardianos/service"
+)
+
+func main() {
+	var root string
+	var pattern string
+
+	flag.StringVar(&root, "dir", "", "The base where the temp dir is located")
+	flag.StringVar(&pattern, "pattern", "tmpfs-*", "The pattern which to create the temp directory name")
+
+	cfg := &service.Config{
+		Name:        "tmpfs",
+		DisplayName: "Temp File System Service",
+		Description: "Provides a 9p file system that exposes a local directory. Defaults to temp directory",
+	}
+
+	dir, err := ioutil.TempDir(root, pattern)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create temp directory: %s", err)
+	}
+	defer func() {
+		if err := os.RemoveAll(dir); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to clean temp directory: %s: %s", dir, err)
+		}
+	}()
+
+	cli.ServiceMain(cfg, func() ninep.FileSystem {
+		fmt.Printf("Serving: %v\n", dir)
+		return fs.Dir(dir)
+	})
+}
