398f2d581273aeacc36154d1fc845a9b25a81b70
Jeff Hui <jeff@jeffhui.net>
2020-04-25 03:13:41 -0700

    cls now hides dot prefixed files by default
    cls now supports -a to list all files. -a will also list info about
    current directory and parent directory.

--- cmd/cls/main.go
+++ cmd/cls/main.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 	"text/tabwriter"
+	"time"
 
 	"github.com/jeffh/cfs/cli"
 	"github.com/jeffh/cfs/ninep"
@@ -20,12 +23,43 @@ const (
 	PB = TB * 1024
 )
 
+var humanSizes bool
+
+func printInfo(w io.Writer, info os.FileInfo, replacedName string) {
+	usr, gid, muid, _ := ninep.FileUsers(info)
+	size := info.Size()
+	var sizeStr string
+	if humanSizes {
+		if size > PB {
+			sizeStr = fmt.Sprintf("%.2fPB", float64(size)/PB)
+		} else if size > TB {
+			sizeStr = fmt.Sprintf("%.2fTB", float64(size)/TB)
+		} else if size > GB {
+			sizeStr = fmt.Sprintf("%.2fGB", float64(size)/GB)
+		} else if size > MB {
+			sizeStr = fmt.Sprintf("%.2fMB", float64(size)/MB)
+		} else if size > KB {
+			sizeStr = fmt.Sprintf("%.2fKB", float64(size)/KB)
+		} else {
+			sizeStr = fmt.Sprintf("%dB", size)
+		}
+	} else {
+		sizeStr = fmt.Sprintf("%d", size)
+	}
+	n := info.Name()
+	if replacedName != "" {
+		n = replacedName
+	}
+	fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t %s\n", info.Mode(), usr, gid, muid, sizeStr, info.ModTime().Format(time.RFC822), n)
+}
+
 func main() {
 	var path string
+	var all bool
 	var list bool
 	var nocols bool
-	var humanSizes bool
 
+	flag.BoolVar(&all, "a", false, "list info of current directory and parent directory and any dot-prefixed files")
 	flag.BoolVar(&list, "l", false, "list long format stats about each file")
 	flag.BoolVar(&humanSizes, "h", false, "list file sizes in human-readable formats")
 	flag.BoolVar(&nocols, "nocols", false, "avoids printing in nice columns, useful to verify streaming behavior")
@@ -59,30 +93,22 @@ func main() {
 				defer tw.Flush()
 				w = tw
 			}
+			if all {
+				info, err := fs.Stat(context.Background(), path)
+				if err == nil && info != nil {
+					printInfo(w, info, ".")
+				}
+				info, err = fs.Stat(context.Background(), filepath.Join(path, ".."))
+				if err == nil && info != nil {
+					printInfo(w, info, "..")
+				}
+			}
 			for {
 				info, err := infos.NextFileInfo()
 				if info != nil {
-					usr, gid, muid, _ := ninep.FileUsers(info)
-					size := info.Size()
-					var sizeStr string
-					if humanSizes {
-						if size > PB {
-							sizeStr = fmt.Sprintf("%.2f PB", float64(size)/PB)
-						} else if size > TB {
-							sizeStr = fmt.Sprintf("%.2f TB", float64(size)/TB)
-						} else if size > GB {
-							sizeStr = fmt.Sprintf("%.2f GB", float64(size)/GB)
-						} else if size > MB {
-							sizeStr = fmt.Sprintf("%.2f MB", float64(size)/MB)
-						} else if size > KB {
-							sizeStr = fmt.Sprintf("%.2f KB", float64(size)/KB)
-						} else {
-							sizeStr = fmt.Sprintf("%d", size)
-						}
-					} else {
-						sizeStr = fmt.Sprintf("%d", size)
+					if all || !strings.HasPrefix(info.Name(), ".") {
+						printInfo(w, info, "")
 					}
-					fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t %s\n", info.Mode(), usr, gid, muid, sizeStr, info.ModTime(), info.Name())
 				}
 				if err == io.EOF {
 					break
@@ -91,10 +117,16 @@ func main() {
 				}
 			}
 		} else {
+			if all {
+				fmt.Println(".")
+				fmt.Println("..")
+			}
 			for {
 				info, err := infos.NextFileInfo()
 				if info != nil {
-					fmt.Println(info.Name())
+					if all || !strings.HasPrefix(info.Name(), ".") {
+						fmt.Println(info.Name())
+					}
 				}
 				if err == io.EOF {
 					break
