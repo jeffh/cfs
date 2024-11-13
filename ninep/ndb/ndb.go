da5e07530c2655aac389772c87c95ac4abf91a23
Jeff Hui <jeff@jeffhui.net>
2024-11-12 17:38:15 -0800

    Rewrite kvp to ndb

---
+++ ninep/ndb/ndb.go
@@ -0,0 +1,316 @@
+package ndb
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"iter"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+type Tuple struct {
+	Attr, Val string
+}
+
+type Record []Tuple
+
+func (r Record) Lookup(attr string) (string, bool) {
+	for _, t := range r {
+		if t.Attr == attr {
+			return t.Val, true
+		}
+	}
+	return "", false
+}
+
+func (r Record) Get(attr string) string {
+	for _, t := range r {
+		if t.Attr == attr {
+			return t.Val
+		}
+	}
+	return ""
+}
+
+func (r Record) GetAll(attr string) []string {
+	var results []string
+	for _, t := range r {
+		if t.Attr == attr {
+			results = append(results, t.Val)
+		}
+	}
+	return results
+}
+
+func (r Record) String() string {
+	var sb strings.Builder
+	for i, t := range r {
+		if i > 0 {
+			sb.WriteString(" ")
+		}
+		sb.WriteString(t.Attr)
+		sb.WriteString("=")
+		sb.WriteString(strconv.Quote(t.Val))
+	}
+	return sb.String()
+}
+
+type Ndb struct {
+	data  [][]byte
+	files []string
+	sys   System
+}
+
+type System interface {
+	Open(path string) (io.ReadCloser, error)
+}
+
+type osSys struct{}
+
+func (osSys) Open(path string) (io.ReadCloser, error) {
+	return os.Open(path)
+}
+
+func Open(sys System, filepath string) (*Ndb, error) {
+	if sys == nil {
+		sys = osSys{}
+	}
+	db := &Ndb{files: []string{filepath}, sys: sys}
+	for {
+		n, err := db.readFiles()
+		if err != nil {
+			return nil, err
+		}
+		if n == 0 {
+			break
+		}
+		for record := range db.Search("database", "") {
+			for _, file := range record.GetAll("file") {
+				if !slices.Contains(db.files, file) {
+					db.files = append(db.files, file)
+				}
+			}
+		}
+	}
+	return db, nil
+}
+
+func OpenOne(sys System, filepath string) (*Ndb, error) {
+	if sys == nil {
+		sys = osSys{}
+	}
+	db := &Ndb{files: []string{filepath}, sys: sys}
+	if _, err := db.readFiles(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func (n *Ndb) readFile(fileToRead string) ([]byte, error) {
+	f, err := n.sys.Open(fileToRead)
+	if err != nil {
+		return nil, err
+	}
+	buf, err := io.ReadAll(f)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	// TODO: validate syntax
+	return buf, nil
+}
+
+func (n *Ndb) readFiles() (int, error) {
+	if len(n.files) == len(n.data) {
+		return 0, nil
+	}
+	count := 0
+	for _, fileToRead := range n.files[len(n.data):] {
+		buf, err := n.readFile(fileToRead)
+		if err != nil {
+			return count, err
+		}
+		n.data = append(n.data, buf)
+		count++
+		// TODO: validate syntax
+	}
+	return count, nil
+}
+
+func (n *Ndb) SearchSlice(attr, val string) []Record {
+	var results []Record
+	for rec := range n.Search(attr, val) {
+		newRecord := make(Record, len(rec))
+		copy(newRecord, rec)
+		results = append(results, newRecord)
+	}
+	return results
+}
+
+func (n *Ndb) Search(attr, val string) iter.Seq[Record] {
+	var results Record
+	return func(yield func(Record) bool) {
+		recBytes := []byte{}
+	loop:
+		for i := range n.data {
+			scanner := bufio.NewScanner(bytes.NewReader(n.data[i]))
+			for scanner.Scan() {
+				line := scanner.Bytes()
+				cIndex := bytes.IndexByte(line, '#')
+				if cIndex != -1 {
+					line = line[:cIndex]
+				}
+				if len(line) == 0 {
+					continue
+				}
+				first, _ := utf8.DecodeRune(line)
+
+				if !unicode.IsSpace(first) {
+					if len(recBytes) > 0 {
+						if hasAttr(recBytes, attr, val) {
+							err := parseRecord(recBytes, &results)
+							if err != nil {
+								// fmt.Printf("parseRecord error: %v\n", err)
+								continue
+							}
+							if !yield(results) {
+								recBytes = recBytes[:0]
+								break loop
+							}
+						}
+					}
+					recBytes = recBytes[:0]
+				}
+				line = bytes.TrimSpace(line)
+				if len(line) > 0 {
+					recBytes = append(recBytes, ' ')
+					recBytes = append(recBytes, line...)
+					recBytes = append(recBytes, ' ')
+				}
+			}
+			if len(recBytes) > 0 {
+				if hasAttr(recBytes, attr, val) {
+					err := parseRecord(recBytes, &results)
+					if err == nil {
+						yield(results)
+					} else {
+						// fmt.Printf("parseRecord error: %v\n", err)
+					}
+				}
+			}
+			recBytes = recBytes[:0]
+		}
+	}
+}
+
+func hasAttr(recBytes []byte, attr, value string) bool {
+	attrKey := []byte(" " + attr + "=")
+	off := 0
+	for off < len(recBytes) {
+		idx := bytes.Index(recBytes[off:], attrKey)
+		if idx == -1 {
+			return len(value) == 0 && bytes.Index(recBytes, []byte(" "+attr+" ")) != -1
+		}
+
+		valueStart := idx + len(attrKey)
+		if len(recBytes) <= valueStart {
+			return value == ""
+		}
+		first, _ := utf8.DecodeRune(recBytes[valueStart:])
+		if first == '"' {
+			length := bytes.IndexAny(recBytes[valueStart:], "\"")
+			if length == -1 {
+				length = len(recBytes) - valueStart
+			}
+			off += idx + valueStart + length
+
+			// TODO: avoid string allocation
+			actualValue, err := strconv.Unquote(string(recBytes[valueStart : valueStart+length]))
+			if err == nil && value == actualValue {
+				return true
+			}
+		} else {
+			length := bytes.IndexAny(recBytes[valueStart:], " \t\r\n")
+			if length == -1 {
+				length = len(recBytes) - valueStart
+			}
+			off += idx + valueStart + length
+
+			if bytes.Equal([]byte(value), recBytes[valueStart:valueStart+length]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func parseRecord(recBytes []byte, results *Record) error {
+	if results != nil {
+		if *results == nil {
+			*results = make(Record, 0, 10)
+		}
+	}
+	r := recBytes
+	for len(r) > 0 {
+		ch, size := utf8.DecodeRune(r)
+		if ch == utf8.RuneError {
+			r = r[size:]
+			return fmt.Errorf("invalid utf8 rune")
+		}
+		if unicode.IsSpace(ch) {
+			r = r[size:]
+			continue
+		}
+		tup, n, err := parseTuple(r)
+		if err == nil {
+			if results != nil {
+				*results = append(*results, tup)
+			}
+		} else {
+			return err
+		}
+		r = r[n:]
+	}
+	return nil
+}
+
+func parseTuple(p []byte) (Tuple, int, error) {
+	equals := bytes.IndexByte(p, '=')
+	if equals == -1 {
+		// look for next whitespace
+		ws := bytes.IndexAny(p, " \t\r\n")
+		if ws == -1 {
+			return Tuple{string(p), ""}, len(p), nil
+		} else {
+			return Tuple{string(p[:ws]), ""}, ws + 1, nil
+		}
+	}
+	attr := string(p[:equals])
+	valueStart := equals + 1
+	firstValue, _ := utf8.DecodeRune(p[valueStart:])
+	if firstValue == '"' {
+		length := bytes.IndexAny(p[valueStart:], "\"")
+		if length == -1 {
+			length = len(p) - valueStart
+		}
+
+		actualValue, err := strconv.Unquote(string(p[valueStart : valueStart+length]))
+		if err != nil {
+			return Tuple{}, 0, err
+		}
+		return Tuple{attr, actualValue}, valueStart + length + 1, nil
+	} else {
+		length := bytes.IndexAny(p[valueStart:], " \t\r\n")
+		if length == -1 {
+			length = len(p) - valueStart
+		}
+
+		return Tuple{attr, string(p[valueStart : valueStart+length])}, valueStart + length + 1, nil
+	}
+}
---
+++ ninep/ndb/ndb_test.go
@@ -0,0 +1,128 @@
+package ndb
+
+import (
+	"io"
+	"io/fs"
+	"strings"
+	"testing"
+)
+
+type memSys struct {
+	tree map[string]string
+}
+
+func (m *memSys) Open(path string) (io.ReadCloser, error) {
+	if s, ok := m.tree[path]; ok {
+		return io.NopCloser(strings.NewReader(s)), nil
+	}
+	return nil, fs.ErrNotExist
+}
+
+func TestSimpleParse(t *testing.T) {
+	m := &memSys{
+		tree: map[string]string{
+			"test.ndb": `givenName=John familyName=Doe # a comment`,
+			"multiple.ndb": `givenName=John familyName=Doe
+givenName=Jane familyName=Doe`,
+			"multiline.ndb": `givenName=John
+	   familyName=Doe
+givenName=Jane familyName=Doe`,
+			"noValue.ndb": `givenName=John familyName=Doe person`,
+		},
+	}
+	t.Run("test.ndb", func(t *testing.T) {
+		db := mustOpenOne(t, m, "test.ndb")
+		records := db.SearchSlice("givenName", "John")
+		if len(records) != 1 {
+			t.Fatalf("expected 1 record, got %d", len(records))
+		}
+		if records[0].Get("familyName") != "Doe" {
+			t.Fatalf("expected familyName to be Doe, got %s", records[0].Get("familyName"))
+		}
+		if records[0].Get("givenName") != "John" {
+			t.Fatalf("expected givenName to be John, got %s", records[0].Get("givenName"))
+		}
+	})
+
+	t.Run("multiple.ndb", func(t *testing.T) {
+		db := mustOpenOne(t, m, "multiple.ndb")
+		records := db.SearchSlice("givenName", "Jane")
+		if len(records) != 1 {
+			t.Fatalf("expected 1 record, got %d", len(records))
+		}
+		records = db.SearchSlice("familyName", "Doe")
+		if len(records) != 2 {
+			t.Fatalf("expected 2 records, got %d", len(records))
+		}
+	})
+
+	t.Run("multiline.ndb", func(t *testing.T) {
+		db := mustOpenOne(t, m, "multiline.ndb")
+		records := db.SearchSlice("givenName", "John")
+		if len(records) != 1 {
+			t.Fatalf("expected 1 record, got %d", len(records))
+		}
+		if records[0].Get("familyName") != "Doe" {
+			t.Fatalf("expected familyName to be Doe, got %s (%#v)", records[0].Get("familyName"), records)
+		}
+		records = db.SearchSlice("familyName", "Doe")
+		if len(records) != 2 {
+			t.Fatalf("expected 2 records, got %d", len(records))
+		}
+	})
+
+	t.Run("noValue.ndb", func(t *testing.T) {
+		println("===========================")
+		db := mustOpenOne(t, m, "noValue.ndb")
+		records := db.SearchSlice("person", "")
+		if len(records) != 1 {
+			t.Fatalf("expected 1 record, got %d", len(records))
+		}
+		if records[0].Get("familyName") != "Doe" {
+			t.Fatalf("expected familyName to be Doe, got %#v", records[0].Get("familyName"))
+		}
+	})
+}
+
+func TestDatabaseParse(t *testing.T) {
+	m := &memSys{
+		tree: map[string]string{
+			"start.ndb": `database=
+	file=doe.ndb
+	file=appleseed.ndb`,
+			"doe.ndb": `givenName=John familyName=Doe
+givenName=Jane familyName=Doe`,
+			"appleseed.ndb": `givenName=John familyName=Appleseed`,
+		},
+	}
+	db := mustOpen(t, m, "start.ndb")
+	records := db.SearchSlice("givenName", "John")
+	if len(records) != 2 {
+		t.Fatalf("expected 2 record, got %d", len(records))
+	}
+	if records[0].Get("familyName") != "Doe" {
+		t.Fatalf("expected familyName to be Doe, got %s", records[0].Get("familyName"))
+	}
+	if records[0].Get("givenName") != "John" {
+		t.Fatalf("expected givenName to be John, got %s", records[0].Get("givenName"))
+	}
+}
+func mustOpen(t *testing.T, sys System, path string) *Ndb {
+	t.Helper()
+	db, err := Open(sys, path)
+	must(t, err)
+	return db
+}
+func mustOpenOne(t *testing.T, sys System, path string) *Ndb {
+	t.Helper()
+	db, err := OpenOne(sys, path)
+	must(t, err)
+	return db
+}
+
+func must(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
