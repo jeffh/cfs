f4b83c2e1fc2868bf1d033fe17d2d92a88ec2ecd
Jeff Hui <jeff@jeffhui.net>
2024-11-12 21:02:53 -0800

    ndb: add docstring

---
+++ ninep/ndb/doc.go
@@ -0,0 +1,7 @@
+// ndb provides methods to reading Plan 9 Network Database (ndb) files.
+//
+// ndb files are a simple key-value storage mechanism that can be used to
+// specify configuration information. The format also supports including other
+// files to facilitate configuration use over different (networked) file
+// systems.
+package ndb
