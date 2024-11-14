13e49be330627d36a5e3cf627e2680fb967346b6
Jeff Hui <jeff@jeffhui.net>
2024-11-13 19:10:52 -0800

    remove old implementation of procfs

--- fs/procfs/main.go
+++ fs/procfs/main.go
@@ -3,13 +3,6 @@ package procfs
 
 import (
 	"errors"
-	"fmt"
-	"os"
-	"os/user"
-	"sort"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/jeffh/cfs/ninep"
 )
@@ -102,116 +95,8 @@ type Fd struct {
 	SocketRemoteAddr string
 }
 
-func procDir(pid Pid) func() ([]ninep.Node, error) {
-	return func() ([]ninep.Node, error) {
-		now := time.Now()
-		pi, err := pidInfo(pid)
-		if err != nil {
-			return nil, err
-		}
-		uid := int(pi.Uid)
-		var username string
-		{
-			usr, err := user.LookupId(strconv.Itoa(uid))
-			if err == nil {
-				username = usr.Username
-			}
-		}
-		gid := int(pi.Gid)
-		var groupname string
-		{
-			grp, err := user.LookupGroupId(strconv.Itoa(gid))
-			if err == nil {
-				groupname = grp.Name
-			}
-		}
-		nodes := []ninep.Node{
-			staticStringFile("pid", now, strconv.Itoa(int(pid))),
-			staticStringFile("status", now, pi.Status.String()),
-			staticStringFile("uid", now, fmt.Sprintf("%d\n%s\n", uid, username)),
-			staticStringFile("gid", now, fmt.Sprintf("%d\n%s\n", gid, groupname)),
-			staticStringFile("real_uid", now, strconv.Itoa(int(pi.RealUid))),
-			staticStringFile("real_gid", now, strconv.Itoa(int(pi.RealGid))),
-			staticStringFile("parent_pid", now, strconv.Itoa(int(pi.ParentPid))),
-			staticStringFile("pid_group", now, strconv.Itoa(pi.PidGroup)),
-			dynamicStringFile("cmdline", now, func() ([]byte, error) {
-				argv, err := pidArgs(pid)
-				if err == nil {
-					for i, arg := range argv[1:] {
-						if strings.Index(arg, " ") != -1 {
-							argv[i+1] = fmt.Sprintf("%#v", arg)
-						}
-					}
-				}
-				return []byte(strings.Join(argv, " ")), err
-			}),
-			dynamicStringFile("env", now, func() ([]byte, error) {
-				env, err := pidEnv(pid)
-				return []byte(strings.Join(env, "\n")), err
-			}),
-			dynamicStringFile("fds", now, func() ([]byte, error) {
-				fds, err := pidFds(pid)
-				str := make([]string, len(fds))
-				for i, fd := range fds {
-					str[i] = fmt.Sprintf("%#v", fd)
-				}
-				return []byte(strings.Join(str, "\n")), err
-			}),
-		}
-		return nodes, nil
-	}
-}
-
-func procList() func() ([]ninep.Node, error) {
-	return func() ([]ninep.Node, error) {
-		pids, err := pidsList(QUERY_ALL)
-		if err != nil {
-			return nil, err
-		}
-		sort.Sort(SortablePids(pids))
-		var nodes []ninep.Node
-		nodes = make([]ninep.Node, len(pids))
-		for i, pid := range pids {
-			nodes[i] = dynamicDir(strconv.Itoa(int(pid)), procDir(pid))
-		}
-		return nodes, nil
-	}
-}
-
 func NewFs() ninep.FileSystem {
 	return &fsys{}
-	// fs := &ninep.SimpleFileSystem{
-	// 	Root: ninep.DynamicRootDir(procList()),
-	// }
-	// return fs
-}
-
-func staticDir(name string, children ...ninep.Node) *ninep.StaticReadOnlyDir {
-	return &ninep.StaticReadOnlyDir{
-		SimpleFileInfo: ninep.SimpleFileInfo{
-			FIName: name,
-			FIMode: os.ModeDir | 0777,
-		},
-		Children: children,
-	}
-}
-
-func dynamicDir(name string, resolve func() ([]ninep.Node, error)) *ninep.DynamicReadOnlyDir {
-	return &ninep.DynamicReadOnlyDir{
-		SimpleFileInfo: ninep.SimpleFileInfo{
-			FIName: name,
-			FIMode: os.ModeDir | 0777,
-		},
-		GetChildren: resolve,
-	}
-}
-
-func staticStringFile(name string, modTime time.Time, contents string) *ninep.SimpleFile {
-	return ninep.StaticReadOnlyFile(name, 0444, modTime, []byte(contents))
-}
-
-func dynamicStringFile(name string, modTime time.Time, content func() ([]byte, error)) *ninep.SimpleFile {
-	return ninep.DynamicReadOnlyFile(name, 0777, modTime, content)
 }
 
 ///////////////////////////////////////
